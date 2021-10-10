67c70d31280a022c4bc6163f0b38efc112a7d4e4
Zair Abbas <zair@grocode.io>
2021-10-10 08:58:40 +0500

    removed jquery

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,16 +5,16 @@
  */
 
 // You can delete this file if you're not using it
-const addScript = url => {
-    const script = document.createElement("script")
-    script.src = url
-    script.async = true
-    document.body.appendChild(script)
-  }
+// const addScript = url => {
+//     const script = document.createElement("script")
+//     script.src = url
+//     script.async = true
+//     document.body.appendChild(script)
+//   }
   
-  export const onClientEntry = () => {
-    window.onload = () => {
-       addScript("https://code.jquery.com/jquery-2.2.4.min.js")
-       addScript("js/app.js")
-    }
-  }
+//   export const onClientEntry = () => {
+//     window.onload = () => {
+//        addScript("https://code.jquery.com/jquery-2.2.4.min.js")
+//        addScript("js/app.js")
+//     }
+//   }
